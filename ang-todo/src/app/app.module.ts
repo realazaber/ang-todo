e8949bcd6923bb92b0811f809fcbf0c7658561fc
swagoverlord <azcooldude8@gmail.com>
2023-02-16 11:38:15 +0100

    Generated all components

--- ang-todo/src/app/app.module.ts
+++ ang-todo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { TodoviewComponent } from './components/tasks/todoview/todoview.component';
+import { TodoComponent } from './components/tasks/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TodoviewComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
