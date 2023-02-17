7f7cfd9f3f2b0d16d2f5e8f87b99a9e09b81f802
swagoverlord <azcooldude8@gmail.com>
2023-02-17 03:14:02 +0100

    Example tasks

--- ang-todo/src/app/components/tasks/todoview/todoview.component.ts
+++ ang-todo/src/app/components/tasks/todoview/todoview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/interfaces/todo';
 
 @Component({
   selector: 'app-todoview',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoviewComponent implements OnInit {
 
+  todos: Todo[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.todos.push(new Todo("Task 1", "Content"));
+    this.todos.push(new Todo("Task 2", "Content"));
+    this.todos.push(new Todo("Task 3", "Content"));
+    this.todos.push(new Todo("Task 4", "Content"));
+    this.todos.push(new Todo("Task 5", "Content"));
+    this.todos.push(new Todo("Task 6", "Content"));
+    this.todos.push(new Todo("Task 7", "Content"));
   }
 
 }
