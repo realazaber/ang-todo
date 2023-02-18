e5962dc79a1099c6776936f0bead9026253370a9
swagoverlord <azcooldude8@gmail.com>
2023-02-18 21:44:13 +0100

    TODOs can now take in props and have basic funcitonality

--- ang-todo/src/app/components/tasks/todo/todo.component.ts
+++ ang-todo/src/app/components/tasks/todo/todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { __classPrivateFieldSet } from 'tslib';
 
 @Component({
   selector: 'app-todo',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+
+  id: number = 0;
+  @Input() title: string = "Sample title";
+  @Input() content: string = "";
+  @Input() completed: boolean = false;
+
+  toggleComplete() {
+    this.completed = !this.completed;
+    if (this.completed) {
+      alert(this.title + " completed!");
+    }
+  }
+
+  deleteSelf() {
+    if (this.completed) {
+      alert(this.title + " deleted!");
+    }
+    else {
+      alert("Not completed yet!");
+    }
+    
+  }
 
   constructor() { }
 
